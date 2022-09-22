062218b9ecf8b2d374dbedcfc29dbeabab81b35d
TobyZiegler <Toby@TobyZiegler.com>
2022-09-21 22:46:11 -0500

    Initial Commit

---
+++ AnnounceTime/AnnounceTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnounceTimeApp.swift
+//  AnnounceTime
+//
+//  Created by TJ Ziegler on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AnnounceTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
